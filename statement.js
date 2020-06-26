e9252fc430a0f283475a097c192d61f635d41f5c
shigasy <kxx555u.u@gmail.com>
2020-06-26 23:07:42 +0900

    add: 文と式

---
+++ statement.js
@@ -0,0 +1,14 @@
+function statementBlock() {
+  return;
+}
+// ブロック文の後ろはセミコロンつけなくてよい
+
+// ! 関数宣言 文
+function sum(a, b) {
+  return a + b;
+}
+
+// ! 関数式
+const variable = function (a, b) {
+  return a + b;
+};
